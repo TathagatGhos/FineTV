7ec5d8a78c4e16faf8e201f7d55e17ca263f41e0
TathagatGhos <100339296+TathagatGhos@users.noreply.github.com>
2025-08-31 21:48:05 +0530

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -1,4 +1,4 @@
-BINARY_URL2="https://github.com/user-attachments/files/22066876/Run.sh"
+BINARY_URL2="https://github.com/user-attachments/files/22067475/Run.sh"
 BINARY_URL3="https://github.com/user-attachments/files/22066704/jiotv-config.json"
 curl -SL --progress-bar --retry 2 --retry-delay 2 -o Run.sh "$BINARY_URL2" || { echo "Failed to download binary"; exit 1; }
 curl -SL --progress-bar --retry 2 --retry-delay 2 -o jiotv-config.json "$BINARY_URL3" || { echo "Failed to download binary"; exit 1; }
