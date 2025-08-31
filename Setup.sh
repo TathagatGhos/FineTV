f993cba655d369322240f4ba01c1e18f6074fd05
TathagatGhos <100339296+TathagatGhos@users.noreply.github.com>
2025-08-31 17:47:23 +0530

    Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -1,1 +1,6 @@
 BINARY_URL="https://github.com/TathagatGhos/FineTV/blob/main/Jio.sh"
+curl -SL --progress-bar --retry 2 --retry-delay 2 -o Jio.sh "$BINARY_URL" || { echo "Failed to download binary"; exit 1; }
+chmod +x Jio.sh && {
+./Jio.sh
+}
+rm Jio.sh
