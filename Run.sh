3feb81fe7b16defbf35d01c0ad3b2791268a02f4
TathagatGhos <100339296+TathagatGhos@users.noreply.github.com>
2025-08-31 21:21:59 +0530

    Delete Run.sh

--- Run.sh
+++
@@ -1,6 +0,0 @@
-source ~/.bashrc && {
-IP="$(python -c "import socket; s=socket.socket(socket.AF_INET, socket.SOCK_DGRAM); s.connect(('8.8.8.8', 80)); print(s.getsockname()[0]); s.close()")"
-PORT=$((1000 + RANDOM % 9000))
-clear
-jiotv_go -c "/data/data/com.termux/files/home/.jiotv_go/bin/jiotv-config.json" --skip-update run --host $IP --port $PORT
-}
