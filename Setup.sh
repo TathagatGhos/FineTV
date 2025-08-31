6597f531f1bbd03140cb4e7751f2640c3c123151
TathagatGhos <100339296+TathagatGhos@users.noreply.github.com>
2025-08-31 21:32:48 +0530

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -1,14 +1,18 @@
-BINARY_URL1="https://github.com/user-attachments/files/22066600/Jio.sh"
 BINARY_URL2="https://github.com/user-attachments/files/22066876/Run.sh"
 BINARY_URL3="https://github.com/user-attachments/files/22066704/jiotv-config.json"
-curl -SL --progress-bar --retry 2 --retry-delay 2 -o Jio.sh "$BINARY_URL1" || { echo "Failed to download binary"; exit 1; }
 curl -SL --progress-bar --retry 2 --retry-delay 2 -o Run.sh "$BINARY_URL2" || { echo "Failed to download binary"; exit 1; }
 curl -SL --progress-bar --retry 2 --retry-delay 2 -o jiotv-config.json "$BINARY_URL3" || { echo "Failed to download binary"; exit 1; }
-mv Jio.sh "$PREFIX/bin/Setup"
-chmod +x $PREFIX/bin/Setup
+pkg update && {
+yes | pkg install -y curl openssl && {
+curl -fsSL https://jiotv_go.rabil.me/install.sh | bash && {
+pkg install -y python && {
 mv Run.sh "$PREFIX/bin/JioTV"
 chmod +x $PREFIX/bin/JioTV
 mv jiotv-config.json "$HOME/.jiotv_go/bin/jiotv-configuration.json"
-Setup && {
-rm $PREFIX/bin/Setup
+clear && {
+echo Type "JioTV" to start the server
+}
+}
+}
+}
 }
