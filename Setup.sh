f2ac2cb1974c555eefba7f3a9adaae353a639a4c
TathagatGhos <100339296+TathagatGhos@users.noreply.github.com>
2025-08-31 19:00:47 +0530

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -1,5 +1,8 @@
 BINARY_URL1="https://github.com/user-attachments/files/22066600/Jio.sh"
+BINARY_URL2="
 curl -SL --progress-bar --retry 2 --retry-delay 2 -o Jio.sh "$BINARY_URL1" || { echo "Failed to download binary"; exit 1; }
+curl -SL --progress-bar --retry 2 --retry-delay 2 -o Jio.sh "$BINARY_URL2" || { echo "Failed to download binary"; exit 1; }
+mv Run.sh "$PREFIX/bin/JioTV"
 mv Jio.sh "$PREFIX/bin/Setup"
 chmod +x $PREFIX/bin/Setup
 Setup && {
